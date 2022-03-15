2ba78c2d713cf7b1d026f7477b402bd504352fec
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-03-15 21:46:11 +0500

    Method Overriding | Dynamic Polymorphism

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -2,36 +2,29 @@
 
 namespace Polymorphism
 {
-    class myClass
+    class abbu
     {
-        public void Show()
+        public virtual void Show()
         {
             int a = 10;
             int b = 15;
             Console.WriteLine(a+b);
         } 
-        public void Show(int a, int b)
+    }
+    class bacha : abbu
+    {
+        public override void Show()
         {
-            Console.WriteLine(a+b);
-        } 
-        public void Show(int a, int b, int c)
-        {
-            Console.WriteLine(a+b+c);
-        } 
-        public void Show(string a, string b)
-        {
-            Console.WriteLine("{0} {1}",a,b);
+            int a = 10;
+            int b = 15;
+            Console.WriteLine(a - b);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            myClass obj = new myClass();
-            obj.Show();
-            obj.Show(5,9);
-            obj.Show("Asgar","Chandiyo");
-            obj.Show(7,7,7);
+            abbu obj = new bacha();
             obj.Show();
             Console.ReadLine();
         }
