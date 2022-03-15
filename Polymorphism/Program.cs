f8e79b1d7839dba7f941f5470497e0c02dbbd675
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-03-15 21:31:07 +0500

    Method Overloading | Static Polymorphism

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Polymorphism
 {
+    class myClass
+    {
+        public void Show()
+        {
+            int a = 10;
+            int b = 15;
+            Console.WriteLine(a+b);
+        } 
+        public void Show(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        } 
+        public void Show(int a, int b, int c)
+        {
+            Console.WriteLine(a+b+c);
+        } 
+        public void Show(string a, string b)
+        {
+            Console.WriteLine("{0} {1}",a,b);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            myClass obj = new myClass();
+            obj.Show();
+            obj.Show(5,9);
+            obj.Show("Asgar","Chandiyo");
+            obj.Show(7,7,7);
+            obj.Show();
+            Console.ReadLine();
         }
     }
 }
